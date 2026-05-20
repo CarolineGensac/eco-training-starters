edaac7867fb5e814f4276a9f72eafad8c4e50512
CarolineGensac <gensac.caroline@gmail.com>
2026-05-20 14:26:37 +0200

    new file lighthouserc

---
+++ .lighthouserc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:5173/'],
+      startServerCommand: 'npm run dev',
+      numberOfRuns: 2,
+      settings: {
+        chromeFlags: '--no-sandbox',
+        preset: 'desktop',
+      },
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+      assertions: {
+        'categories:performance': ['error', { minScore: 0.6 }],
+        'categories:accessibility': ['warn', { minScore: 0.8 }],
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
